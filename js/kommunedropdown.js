b81b751fbc29ded3ced8ceddc4edb878d23a0dcd
erlm <erlm@kea.dk>
2023-09-13 11:05:49 +0200

    nu atag

--- js/kommunedropdown.js
+++ js/kommunedropdown.js
@@ -2,15 +2,53 @@ console.log("jeg er i kommunedropdown")
 
 const urlKommune = "https://api.dataforsyningen.dk/kommuner";
 
+pbFetchKommuner = document.getElementById("pbFetchKommuner")
+const ddKommuner = document.getElementById("ddKommuner")
+const divTag = document.getElementById("atags")
+
 function fetchAnyUrl(url) {
     console.log("inside fetch url=" + url)
     return  fetch(url).then(response => response.json());
 }
 
+function fillDropdownObj(item) {
+    const el = document.createElement("option")
+    console.log(item)
+    el.textContent = item.navn
+    el.value = item  //item bliver til string
+    el.kommune = item
+    ddKommuner.appendChild(el);
+
+
+}
+
+function inputChanged(key) {
+    const selindex = ddKommuner.selectedIndex;
+    const selectedOption = ddKommuner.options[selindex];
+    const kommune = selectedOption.value //kan ikke bruges er lavet om til en string
+    console.log(kommune)
+    const kom2 = selectedOption.kommune
+    console.log(kom2)
+    createATag(kom2)
+}
+
+function createATag(komObj) {
+    console.log("her kommer obj")
+    console.log(komObj);
+    const aTag = document.createElement("a");
+    console.log(komObj.href)
+    aTag.setAttribute('href', komObj.href);
+    aTag.innerText = komObj.navn;
+    divTag.appendChild(aTag)
+    const brTag = document.createElement('br');
+    divTag.appendChild(brTag);
+}
+
 async function fetchKommuner(urlKommune) {
     let kommuneArr = await fetchAnyUrl(urlKommune)
-    console.log(jsonOutput)
-    kommuneArr.forE
+    console.log(kommuneArr)
+    ddKommuner.innerHTML = ""
+    kommuneArr.forEach(fillDropdownObj)
 }
 
 function actionFetch() {
@@ -18,6 +56,8 @@ function actionFetch() {
     console.log(kommuner)
 }
 
-pbFetchKommuner = document.getElementById("pbFetchKommuner")
+
 pbFetchKommuner.addEventListener('click', actionFetch)
+ddKommuner.addEventListener('change', inputChanged)
+
 
